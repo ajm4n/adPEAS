a763042409e05454920ae1928eb40c62487cbb4d
AJ Hammond <aj.hammond@praetorian.com>
2024-03-15 14:12:27 -0400

    c

--- adPEAS/adPEAS.py
+++ adPEAS/adPEAS.py
@@ -1,17 +1,17 @@
-from impacket.krb5 import constants
+from impacket.krb5 import constants, types
 from impacket.krb5.asn1 import AP_REQ
 from impacket.krb5.ccache import CCache
 from impacket.krb5.types import Principal
 from impacket.smbconnection import SMBConnection
-from impacket.examples import logger
-from impacket import kerberos
 
 def kerberos_auth(username, password, domain, dc_ip):
     try:
+        # Connect to the Domain Controller via SMB
+        smb = SMBConnection(dc_ip, dc_ip)
+        smb.login(username, password, domain)
 
-        # Request a TGT directly
-        krb_cred = kerberos.KerberosCredential(username=username, password=password, domain=domain)
-        krbtgt, krbctx = krb_cred.getKerberosTGT()
+        # Get TGT from KDC
+        krbtgt, krbctx = smb.getKerberosTGT(username, password, domain)
 
         return krbtgt, krbctx
 
