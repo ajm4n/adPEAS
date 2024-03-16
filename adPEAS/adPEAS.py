4647124f330ae7113d38c3b8063ecd01a44f4107
AJ Hammond <aj.hammond@praetorian.com>
2024-03-16 00:23:10 -0400

    adding delegation

--- adPEAS/adPEAS.py
+++ adPEAS/adPEAS.py
@@ -16,6 +16,20 @@ def certipy(username, password, domain, dc_ip):
      except Exception as e:
         print(f"Error while running Certipy: {e}")
 
+def findDelegation(username, password, domain, dc_ip):
+     try:
+          cmd - f"findDelegation.py -dc-ip {dc_ip} {domain}/{username}:{password}"
+          subprocess.run(cmd, Shell=True)
+     except Exception as e:
+          print(f"Error while finding delegation: {e}")
+
+def findUserDelegation(username, password, domain, dc_ip):
+     try:
+          cmd - f"findDelegation.py -user {username} -dc-ip {dc_ip} {domain}/{username}:{password}"
+          subprocess.run(cmd, Shell=True)
+     except Exception as e:
+          print(f"Error while finding user delegation: {e}")
+
 # Example usage:
 username = input("Enter username: ")
 password = input("Enter password: ")
@@ -29,5 +43,10 @@ print("Kerberoasting done!")
 print("Attempting to find all ADCS infrastructure...")
 certipy(username, password, domain, dc_ip)
 print("Done finding all ADCS infrastructure")
-
+print("Attempting to find all delegation...")
+findDelegation(username, password, domain, dc_ip)
+print("Done finding all delegation.")
+print("Attempting to find delegation for your user...")
+findUserDelegation(username, password, domain, dc_ip)
+print("Done finding user delegation.")
 #todo: auto open certipy output and grep for ESCs 
