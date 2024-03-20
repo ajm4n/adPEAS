19fb0a52272fb8e98acd261845c8ba6d8da22987
bernardyip <bernard.yip@praetorian.com>
2024-03-21 04:58:50 +0800

    convert prompts to argparse

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Quick Start
-usage: adPEAS
+```bash
+usage: adPEAS [-h] -u USERNAME [-p PASSWORD] -d DOMAIN -i DC_IP
+
+options:
+  -h, --help            show this help message and exit
+  -u USERNAME, --username USERNAME
+                        Username for log in.
+  -p PASSWORD, --password PASSWORD
+                        Password for log in. Will prompt if not specified.
+  -d DOMAIN, --domain DOMAIN
+                        Domain of the DC.
+  -i DC_IP, --dc-ip DC_IP
+                        Domain Controller IP or hostname.
+```
 
 # Installation
 Install `adPEAS` and its dependencies
--- adPEAS/adPEAS.py
+++ adPEAS/adPEAS.py
@@ -1,4 +1,6 @@
+import argparse
 import subprocess
+import getpass
 from ldap3 import Server, Connection, SUBTREE
 
 def find_and_kerberoast_objects(username, password, domain, dc_ip):
@@ -30,14 +32,24 @@ def bloodhound(username, password, domain, dc_ip):
      except Exception as e:
           print(f"Error running BloodHound: {e}")
 
-def main():
-     # Example usage:
-     username = input("Enter username: ")
-     password = input("Enter password: ")
-     domain = input("Enter domain: ")
-     dc_ip = input("Enter domain controller IP or hostname: ")
+def main(arguments=None):
+     parser = argparse.ArgumentParser("adPEAS")
+     parser.add_argument("-u", "--username", required=True, help="Username for log in.")
+     parser.add_argument("-p", "--password", help="Password for log in. Will prompt if not specified.")
+     parser.add_argument("-d", "--domain", required=True, help="Domain of the DC.")
+     parser.add_argument("-i", "--dc-ip", required=True, help="Domain Controller IP or hostname.")
+     if arguments is None:
+          args = parser.parse_args()
+     else:
+          args = parser.parse_args(arguments)
 
-     print("Welcome to adPEAS v1.0.0!")
+     print("Welcome to adPEAS v1.0.0!")     
+
+     domain = args.domain
+     dc_ip = args.dc_ip
+     username = args.username
+     password = args.password if args.password else getpass.getpass()
+
      print("Attempting to kerberoast the domain...")
      find_and_kerberoast_objects(username, password, domain, dc_ip)
      print("Kerberoasting done!")
