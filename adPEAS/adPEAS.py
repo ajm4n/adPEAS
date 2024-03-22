c4f12533bea1f3a9813a8a51f7a9f9ac618c0ea7
ajman <ajman@AJMAN-PC>
2024-03-21 23:06:13 -0400

    fixed output

--- adPEAS/adPEAS.py
+++ adPEAS/adPEAS.py
@@ -19,7 +19,7 @@ def find_and_kerberoast_objects(username, password, domain, dc_ip):
 def certipy(username, password, domain, dc_ip):
      try:
           cmd = f"certipy find -u {username}@{domain} -p {password} -dc-ip {dc_ip} -enabled -vulnerable -stdout"
-          subprocess.run(cmd, shell=True, captrue_output=True, text=True).stdout
+          output = subprocess.run(cmd, shell=True, captrue_output=True, text=True).stdout
           vuln_pattern = r'ESC([0-9]+)\s+:\s+(.+)'
           cert_pattern = r'Certificate Name\s+:\s+(.+)\n\s+Certificate Authorities\s+:\s+(.+)'
           vulnerabilities = re.findall(vuln_pattern, output)
