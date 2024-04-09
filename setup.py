e463070d32468077e99cd04364551b455b1c86d5
ajman <ajman@AJMAN-PC>
2024-04-08 22:01:46 -0400

    add comma lol

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='adPEAS',
           'impacket==0.11.0',
           'ldap3==2.9.1',
           'importlib-metadata >= 1.0 ; python_version < "3.8"',
-          'regex==2023.12.25'
+          'regex==2023.12.25',
           'certi.py @ git+https://github.com/zer1t0/certi@main'
       ],
       entry_points={
