0bfcedc9f62bb553af6aa7d12ea389ef86cc82b7
AJ Hammond <60402150+ajm4n@users.noreply.github.com>
2025-03-31 23:44:17 -0400

    Update setup.py, fix install issues

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(name='adPEAS',
       license='MIT',
       packages=['adPEAS'],
       install_requires=[
-          'certipy-ad==4.8.2',
-          'bloodhound==1.7.2',
+          'certipy-ad>=4.8.2',
+          'bloodhound>=1.7.2',
           'impacket',
           'ldap3==2.9.1',
           'importlib-metadata >= 1.0 ; python_version < "3.8"',
@@ -19,6 +19,7 @@ setup(name='adPEAS',
           'certi @ git+https://github.com/zer1t0/certi@main',
           'NetExec @ git+https://github.com/Pennyw0rth/NetExec@main',
           'termcolor==2.5.0'
+          'beautifulsoup4'
       ],
       entry_points={
         'console_scripts': ['adPEAS=adPEAS.command_line:main']
